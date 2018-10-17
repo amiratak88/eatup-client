a186a91625386d66305e4061fdabffd52e4fbebb
Amirata Khodaparast <amiratak88@gmail.com>
2018-10-17 04:29:04 -0400

    Add order1 component -- no functionality yet

--- src/components/User/FoodItem.js
+++ src/components/User/FoodItem.js
@@ -7,7 +7,17 @@ class FoodItem extends Component {
 			<Container>
 				<Item.Group>
 				<Item>
-					<Item.Image size='small' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhqvo8sceTqPRKBciFaibhsdcZH-46unwOvMsd8GjIgxPKo9-z' />
+						<Container>
+						<Item.Image size='small' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhqvo8sceTqPRKBciFaibhsdcZH-46unwOvMsd8GjIgxPKo9-z' />
+						<div>
+							<br />
+							<Button negative icon='minus' size='mini'/>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<span>2</span>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<Button positive icon='plus' size='mini'/>
+						</div>
+						</Container>
 
 					<Item.Content>
 						<Item.Header as='a'>Rice</Item.Header>
@@ -18,13 +28,11 @@ class FoodItem extends Component {
 							Tempor sint nisi consectetur elit excepteur duis cillum et adipisicing et. Esse culpa ea laboris incididunt nulla. Aliquip nisi dolore cupidatat do veniam tempor velit veniam mollit mollit consequat elit ipsum. Officia ex ut et est velit ea voluptate dolor sunt eiusmod. Elit id esse quis deserunt pariatur consequat laboris duis ea. Nisi enim dolore nisi esse eu deserunt ipsum voluptate Lorem est aute anim. Mollit tempor nostrud reprehenderit consectetur occaecat dolor.
 						</Item.Description>
 						<Item.Extra>
-						<Button primary floated='left'>
-							Order Now
-						</Button>
 						</Item.Extra>
 					</Item.Content>
 				</Item>
 				</Item.Group>
+				<br />
 			</Container>
 		)
 	}
