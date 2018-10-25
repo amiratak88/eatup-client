dcd3464d4dc76d3b75e589733acdbe44524eb8bf
Amirata Khodaparast <amiratak88@gmail.com>
2018-10-25 08:26:06 -0400

    Add 'initializeOrder' and 'addItem' adapters inside 'userAdapter'

--- src/adapters/userAdapter.js
+++ src/adapters/userAdapter.js
@@ -22,3 +22,21 @@ export const searchRestaurantsAdapter = (term) => {
 	return fetch(`${API_ROOT}/restaurants/search?term=${term}`)
 		.then(res => res.json())
 }
+
+export const initializeOrder = (data) => {
+	return fetch(`${API_ROOT}/orders`, {
+		method: 'POST',
+		headers: HEADERS,
+		body: JSON.stringify(data)
+	})
+		.then(res => res.json())
+}
+
+export const addItem = (data) => {
+	return fetch(`${API_ROOT}/order_items`, {
+		method: 'POST',
+		headers: HEADERS,
+		body: JSON.stringify(data)
+	})
+		.then(res => res.json())
+}
