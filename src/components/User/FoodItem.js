4e189de482006ccf9f292aef3d35926323c3d8ec
Amirata Khodaparast <amiratak88@gmail.com>
2018-10-25 10:20:29 -0400

    Changed 'FoodItem' to order the data cached from the back-end

--- src/components/User/FoodItem.js
+++ src/components/User/FoodItem.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react'
 import { Container, Item, Button } from 'semantic-ui-react'
+import { defaultImgURL } from '../../constants/index'
 
 class FoodItem extends Component {
 	render() {
+		const { item: { imgURL, name, price, description } } = this.props
 		return (
 			<Container>
 				<Item.Group>
-				<Item>
+					<Item>
 						<Container>
-						<Item.Image size='small' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhqvo8sceTqPRKBciFaibhsdcZH-46unwOvMsd8GjIgxPKo9-z' />
-						<div>
-							<br />
-							<Button color='red' icon='minus' size='mini' inverted/>
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-							<span>2</span>
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-							<Button color='green' icon='plus' size='mini' inverted/>
-						</div>
+							<Item.Image
+								size='small'
+								src={imgURL ? imgURL : defaultImgURL }
+							/>
+							<div>
+								<br />
+								<Button color='red' icon='minus' size='mini' inverted/>
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								<span>0</span>
+								&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+								<Button color='green' icon='plus' size='mini' inverted/>
+							</div>
 						</Container>
 
 					<Item.Content>
-						<Item.Header as='a'>Rice</Item.Header>
+						<Item.Header as='a'>{name}</Item.Header>
 						<Item.Meta>
-							<span className='cinema'>$24.00</span>
+							<span className='cinema'>${price}</span>
 						</Item.Meta>
 						<Item.Description>
-							Tempor sint nisi consectetur elit excepteur duis cillum et adipisicing et. Esse culpa ea laboris incididunt nulla. Aliquip nisi dolore cupidatat do veniam tempor velit veniam mollit mollit consequat elit ipsum. Officia ex ut et est velit ea voluptate dolor sunt eiusmod. Elit id esse quis deserunt pariatur consequat laboris duis ea. Nisi enim dolore nisi esse eu deserunt ipsum voluptate Lorem est aute anim. Mollit tempor nostrud reprehenderit consectetur occaecat dolor.
+							<Container>
+								{description}
+							</Container>
 						</Item.Description>
 						<Item.Extra>
 						</Item.Extra>
