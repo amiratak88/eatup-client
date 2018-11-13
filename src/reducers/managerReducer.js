3303c208e90ed3be875f9d7a6c0023e88d619080
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-13 16:09:36 -0500

    Add 'CONFIRM_ORDER' reducer

--- src/reducers/managerReducer.js
+++ src/reducers/managerReducer.js
@@ -1,11 +1,22 @@
 const initialState = {
-	new_orders: []
+	new_orders: [],
+	confirmed_orders: []
 }
 
 export default (state = initialState, action) => {
 	switch(action.type) {
 		case 'GET_MANAGER_DATA':
 			return {...action.payload}
+		case 'CONFIRM_ORDER':
+			const new_orders = [...state.new_orders]
+			const targetOrder = new_orders.find(o => o.id === action.payload)
+			const targetOrderIndex = new_orders.indexOf(targetOrderIndex)
+			targetOrder.status = "confirmed"
+			return {
+				...state,
+				new_orders: new_orders.slice(0, targetOrderIndex).concat(new_orders.slice(targetOrderIndex + 1)),
+				confirmed_orders: [...state.confirmed_orders, targetOrder]
+			}
 		default:
 			return state
 	}
