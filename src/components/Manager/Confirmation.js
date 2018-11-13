09494f7261afadbdb949f7dd5b71929598e2f782
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-13 14:15:36 -0500

    Implement logic for successfully opening and closing <Confirmation />

--- src/components/Manager/Confirmation.js
+++ src/components/Manager/Confirmation.js
@@ -5,19 +5,15 @@ import Receipt from './Receipt'
 
 
 export default class Confirmation extends Component {
-	state = {
-		open: true
-	}
 
-	handleClose = () => this.setState({ open: false })
+	confirm = () => {
+		this.props.toggleConfirmation()
+	}
 
 	render() {
 		return (
-			<Modal
-				open={this.state.open}
-				size='small'
-			>
-					<Header textAlign='center' content='Pickup Order' />
+			<Modal size='small' open={this.props.showConfirmation.toString()}>
+				<Header textAlign='center' content='Pickup Order' />
 				<Modal.Content>
 					<Container textAlign='center'>
 						Received on 1/1/2018 at 4:30 pm
@@ -33,30 +29,35 @@ export default class Confirmation extends Component {
 							onClick={this.handleClose}
 							inverted
 							content='0 - 10 mins'
+							onClick={this.confirm}
 						/>
 						<Button
 							primary
 							onClick={this.handleClose}
 							inverted
 							content='10 - 15 mins'
+							onClick={this.confirm}
 						/>
 						<Button
 							primary
 							onClick={this.handleClose}
 							inverted
 							content='15 - 30 mins'
+							onClick={this.confirm}
 						/>
 						<Button
 							primary
 							onClick={this.handleClose}
 							inverted
 							content='30 - 45 mins'
+							onClick={this.confirm}
 						/>
 						<Button
 							primary
 							onClick={this.handleClose}
 							inverted
 							content='over 45 mins'
+							onClick={this.confirm}
 						/>
 					</Container>
 				</Modal.Actions>
--- src/components/Manager/NewOrder.js
+++ src/components/Manager/NewOrder.js
@@ -1,28 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Item } from 'semantic-ui-react'
 import Confirmation from './Confirmation'
 
-export default () => (
-	<Item.Group fluid="true">
-		{/* <Confirmation /> */}
-		<Item>
-			<Item.Image size='tiny' src='http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/user-male-icon.png' />
+export default class NewOrder extends Component {
 
-			<Item.Content>
-				<Item.Header as='a'>Amirata Khodaparast</Item.Header>
-				<Item.Description>
-					<p>5 items</p>
-					<p>Received 2 minutes ago</p>
-				</Item.Description>
-				<Item.Extra>
+	state = {
+		showConfirmation: false
+	}
 
-				</Item.Extra>
-			</Item.Content>
-			<Button
-				positive
-				icon='check'
-				content='Confirm'
-			/>
-		</Item>
-	</Item.Group>
-)
+	toggleConfirmation = () => {
+		this.setState({ showConfirmation: !this.state.showConfirmation})
+	}
+
+	render() {
+		const {
+			order: {
+				user: {
+					first_name,
+					last_name
+				},
+				order_items
+			}
+		} = this.props
+
+		return (
+			<Item.Group fluid="true">
+				{
+					this.state.showConfirmation && 
+					<Confirmation
+						order={this.props.order}
+						showConfirmation={this.state.showConfirmation}
+						toggleConfirmation={this.toggleConfirmation}
+					/>
+				}
+				<Item>
+					<Item.Image size='tiny' src='http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/user-male-icon.png' />
+
+					<Item.Content>
+						<Item.Header as='a'>{first_name + " " + last_name}</Item.Header>
+						<Item.Description>
+							<p>{order_items.length} items</p>
+							<p>Received 2 minutes ago</p> {/* Substitute with real data */}
+						</Item.Description>
+						<Item.Extra>
+
+						</Item.Extra>
+					</Item.Content>
+					<Button
+						positive
+						icon='check'
+						content='Confirm'
+						onClick={this.toggleConfirmation}
+					/>
+				</Item>
+			</Item.Group>
+		)
+	}
+}
