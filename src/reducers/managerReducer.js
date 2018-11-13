4db6d87b40c116c37b7182402abf0efea703e607
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-12 22:31:28 -0500

    Add 'GET_MANAGER_DATA' reducer

--- src/reducers/managerReducer.js
+++ src/reducers/managerReducer.js
@@ -1,10 +1,12 @@
 const initialState = {
-	
+	orders: []
 }
 
 export default (state = initialState, action) => {
 	switch(action.type) {
+		case 'GET_MANAGER_DATA':
+			return {...action.payload}
 		default:
-			return initialState
+			return state
 	}
 }
