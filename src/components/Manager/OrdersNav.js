351674884e170589a9f60c788f6176457104639e
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-13 22:15:21 -0500

    Add links to OrdersNav to show new and confirmed orders

--- src/components/Manager/OrdersNav.js
+++ src/components/Manager/OrdersNav.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import { Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export default class OrdersNav extends Component {
-	state = { activeItem: 'New' }
+	state = { activeItem: '' }
 
-	handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+	componentDidMount() {
+		const pathName = this.props.history.location.pathname
+		this.setState({activeItem: pathName.slice(10)})
+	}
 
 	render() {
 		const { activeItem } = this.state
 
 		return (
 			<Menu tabular>
-				<Menu.Item name='New' active={activeItem === 'New'} onClick={this.handleItemClick} />
-				<Menu.Item name='Confirmed' active={activeItem === 'Confirmed'} onClick={this.handleItemClick} />
+				<Link to='/managers/new_orders'>
+					<Menu.Item name='New' active={activeItem === 'new_orders'} />
+				</Link>
+				<Link to='/managers/confirmed_orders'>
+					<Menu.Item name='Confirmed' active={activeItem === 'confirmed_orders'} />
+				</Link>
 			</Menu>
 		)
 	}
