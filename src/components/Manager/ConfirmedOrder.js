efa513d2536108f962ea0d627b5ad901c37cff6e
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-13 22:16:05 -0500

    Render the right data in ConfimedOrder component instead of the placeholders

--- src/components/Manager/ConfirmedOrder.js
+++ src/components/Manager/ConfirmedOrder.js
@@ -1,36 +1,59 @@
-import React from 'react'
+import React,{ Component } from 'react'
 import { Button, Item } from 'semantic-ui-react'
 import View from './View'
 
-export default () => (
-	<Item.Group fluid>
-		<View />
-		<Item>
-			<Item.Image size='tiny' src='http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/user-male-icon.png' />
+class ConfirmedOrder extends Component {
 
-			<Item.Content>
-				<Item.Header as='a'>Amirata Khodaparast</Item.Header>
-				<Item.Description>
-					<p>5 items</p>
-					<p>Received 2 minutes ago</p>
-					<p>Promised by 4:30 pm - 4:45 pm</p>
-				</Item.Description>
-			</Item.Content>
-			<Button
-				positive
-				icon='check'
-				content='Ready for Pickup'
-			/>
-			<Button
-				primary
-				icon='eye'
-				content='View'
-			/>
-			<Button
-				color='pink'
-				icon='print'
-				content='Reprint'
-			/>
-		</Item>
-	</Item.Group>
-)
+	state = {
+		showView: false
+	}
+
+	toggleView = () => this.setState({ showView: !this.state.showView})
+
+	render() {
+		const {
+			order: {
+				user: { first_name, last_name },
+				order_items
+			}
+		} = this.props
+
+		return (
+			<Item.Group fluid>
+				{this.state.showView && <View />}
+				<Item>
+					<Item.Image size='tiny' src='http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/user-male-icon.png' />
+
+					<Item.Content>
+						<Item.Header as='a'>
+							{first_name + " " + last_name}
+						</Item.Header>
+						<Item.Description>
+							<p>{order_items.length} items</p>
+							<p>Received 2 minutes ago</p>
+							<p>Promised by 4:30 pm - 4:45 pm</p>
+						</Item.Description>
+					</Item.Content>
+					<Button
+						positive
+						icon='check'
+						content='Ready for Pickup'
+					/>
+					<Button
+						primary
+						icon='eye'
+						content='View'
+						onClick={this.toggleView}
+					/>
+					<Button
+						color='pink'
+						icon='print'
+						content='Reprint'
+					/>
+				</Item>
+			</Item.Group>
+		)
+	}
+}
+
+export default ConfirmedOrder
--- src/components/Manager/ConfirmedOrders.js
+++ src/components/Manager/ConfirmedOrders.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 import { Container } from 'semantic-ui-react'
+import ManagerNav from './ManagerNav'
+import OrdersNav from './OrdersNav'
 import ConfirmedOrder from './ConfirmedOrder'
+import { connect } from 'react-redux' 
 
-export default class ConfirmedOrders extends Component {
+class ConfirmedOrders extends Component {
+
+	getConfirmedOrders() {
+		return this.props.confirmed_orders.map(o => {
+			return <ConfirmedOrder order={o} />
+		})
+	}
+
 	render() {
 		return (
 			<Container textAlign='left'>
-				<ConfirmedOrder />
-				<ConfirmedOrder />
-				<ConfirmedOrder />
-				<ConfirmedOrder />
-				<ConfirmedOrder />
+				<ManagerNav history={this.props.history} />
+				<OrdersNav history={this.props.history} />
+				{this.getConfirmedOrders()}
 			</Container>
 		)
 	}
 }
+
+const mapStateToProps = (state) => ({
+	confirmed_orders: state.manager.confirmed_orders
+})
+
+export default connect(mapStateToProps)(ConfirmedOrders)
