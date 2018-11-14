34f6de9d9d84431d6d45cff4f309428991e0716b
Amirata Khodaparast <amiratak88@gmail.com>
2018-11-13 22:51:00 -0500

    Make addressFormatter helper function

--- src/helpers.js
+++ src/helpers.js
@@ -2,3 +2,11 @@ export const phoneNumberFormatter = (num) => {
 	const numS = num.toString()
 	return `(${numS.slice(0, 3)}) ${numS.slice(3, 6)} - ${numS.slice(6)}`
 }
+
+export const addressFormatter = (addressObj) => {
+	return `
+		${addressObj.line1},
+		${addressObj.city},
+		${addressObj.state + " " + addressObj.zipcode}
+	`
+}
